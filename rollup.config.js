94d75da13d4d1a08fdbee8f14f382e9fa39ae734
temihren <gusartus@gmail.com>
2020-10-22 02:54:18 +0300

    WebSockets is a future

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import preprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -37,36 +38,21 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
 			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
 			css: css => {
 				css.write('bundle.css');
-			}
+			},
+			preprocess: [
+				preprocess.scss({sourceMap: true})
+			]
 		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
 		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
 		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
 		production && terser()
 	],
 	watch: {
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,87 @@
 <script>
-	let name = 'world';
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+
+	let message = '';
+	let allMessages = [];
+
+	let socket = new WebSocket("ws://localhost:8080/messages");
+	socket.onmessage = (e) => {allMessages = JSON.parse(e.data)}
+
+	const sendMessage = () => {
+		if (message) {
+			socket.send(message);
+			message = '';
+		}
+	}
+
+	const handleEnter = (e) => {
+		if (e.key === 'Enter') {
+			socket.send(message);
+			message = '';
+		}
+	}
+
+	onMount(() => {
+		axios.get('/getLastMessages').then(res => {allMessages = res.data});
+	});
+
 </script>
 
-<h1>Hello {name}!</h1>
+<div class='main'>
+	<div class='messages'>
+		{#each allMessages as msg}
+			<div>{msg}</div>
+		{/each}
+	</div>
+	<div class='input_wrapper'>
+		<input type="text" class='input' bind:value={message} on:keypress={handleEnter}>
+		<div class='send_message'>
+			<span on:click={sendMessage}>Send</span>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.main {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.messages {
+		flex-grow: 1;
+	}
+
+	.input_wrapper {
+		width: 100%;
+		height: 40px;
+		position: relative;
+		display: flex;
+
+		& .input {
+			width: 100%;
+			height: 100%;
+			padding: 10px;
+			font-size: 20px;
+			border-radius: 0;
+			flex-grow: 1;
+			border: 4px solid black;
+		}
+
+		& .send_message {
+			cursor: pointer;
+			flex-shrink: 0;
+			width: 100px;
+			height: 100%;
+			background-color: black;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 20px;
+			color: white;
+			font-weight: 400;
+		}
+	}
+</style>
